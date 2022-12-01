2a253d33c103d885f423a1046138d1dbc4216cec
Rounak Datta <rounakdatta12@gmail.com>
2022-12-01 13:11:04 +0530

    Add day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.fold(Pair(0, 0)) { (currentSum, maxSum), it ->
+            if (it != "") {
+                Pair(currentSum + it.toInt(), max(maxSum, currentSum + it.toInt()))
+            } else {
+                Pair(0, maxSum)
+            }
+        }
+            .second
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.toMutableList().apply { this.add("") }
+            .fold(Pair(0, mutableListOf(0))) { (currentSum, listOfSums), it ->
+                if (it != "") {
+                    Pair(currentSum + it.toInt(), listOfSums)
+                } else {
+                    Pair(0, listOfSums.apply { this.add(currentSum) })
+                }
+            }
+            .second.sorted()
+            .takeLast(3)
+            .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
